eae2234b68db708ccf81eba7e579e82fda5298b8
Ravi Kumar Singh <ravikrsngh.rks@gmail.com>
2022-10-27 03:06:21 +0530

    all set

--- InsectFarming/settings.py
+++ InsectFarming/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mvsqs%m&04#byc7eq5womnic_w5vry94d7+3qtaoe_&rk)(ol9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','insectfarmingbusiness.us-west-1.elasticbeanstalk.com']
 
 
 # Application definition
